7bb45d4dfe908e0f729d58e6dfaf0a4443b48a51
mingmaomak <mingmaomak@gmail.com>
2021-07-25 23:05:27 +0700

    idk

--- App.js
+++ App.js
@@ -2,19 +2,31 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Constants from 'expo-conststants';
+import { NavigationContainer } from '@react-navigation/native'; 
+import { createStackNavigator } from '@react-navigation/stack'; 
+ 
+const Stack = createStackNavigator();
 
 export default function App() {
   const doIt = () => {
     console.log("Hello from console")
   }
-  return (
+  /* return (
     <View style={styles.container}>
-      {/* <Text>Open up App.js to start working on your app!</Text> */}
-      {/* <text onpress={doIt}> Hello world </text> */}
+      { <Text>Open up App.js to start working on your app!</Text> }
+      { <text onpress={doIt}> Hello world </text> }
       <Weather zipCode = "90110"/>
-      <StatusBar style="auto" /> {/* ตั้งสี status bar ให้ตรงกับสีของแอป */}
+      <StatusBar style="auto" /> { ตั้งสี status bar ให้ตรงกับสีของแอป }
     </View>
-  );
+  ); */
+  return(
+    <NavigationContainer> 
+      <Stack.Navigator> 
+        <Stack.Screen name="Home" component={ZipCodeScreen} /> 
+      </Stack.Navigator>  
+    </NavigationContainer>        
+  ); 
+}
 }
 
 const styles = StyleSheet.create({
