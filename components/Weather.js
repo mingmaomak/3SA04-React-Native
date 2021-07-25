b2e8e23eaa2b9cfcda8490a0d70648bbe2d6ac80
mingmaomak <mingmaomak@gmail.com>
2021-07-25 19:49:27 +0700

    Changes to be committed: 	new file:   components/Weather.js  Changes not staged for commit: 	modified:   App.js

---
+++ components/Weather.js
@@ -0,0 +1,9 @@
+//import React from 'core-js/library/fn/reflect/es7/metadata'
+import React from 'react'
+
+export default function Weather(props){
+    return(
+        <Text> {props.zipCode} </Text>
+    )
+}
+
